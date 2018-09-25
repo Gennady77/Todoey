4d38b413e07b2b3746c6e7b8c4bbfa533c51deab
gennadii_kondratev <gennadii.kondratev@externals.adidas.com>
2018-09-25 15:21:20 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Gennadii Kondratev on 25/09/2018.
+//  Copyright © 2018 Gennadii Kondratev. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
